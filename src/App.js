887b3dfb8270432d42f6b30adf5e33610436d3b2
briz9343 <brijeshmavani2002@gmail.com>
2022-12-12 09:41:36 +0530

    text1 git commit -m text1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload on react.
+          Edit <code>src/App.js</code> and save to reload on react-lite-1.
         </p>
         <a
           className="App-link"
