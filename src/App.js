3ea6240a04bac7c638c70fcb4170ec5d12b1e29d
briz9343 <brijeshmavani2002@gmail.com>
2022-12-16 09:17:42 +0530

    reduce-map

--- src/App.js
+++ src/App.js
@@ -1,87 +1,96 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import data from './data.json';
-import { isLabelWithInternallyDisabledControl } from '@testing-library/user-event/dist/utils';
+// import data from './data.json';
+// import { isLabelWithInternallyDisabledControl } from '@testing-library/user-event/dist/utils';
 
 function App() {
 
   const myFunc = [
     {
-      name: "Mobile",
-      ProductId: 1,
-      price: 20000
+      id: 101,
+      name: 'Abacavir',
+      quantity: 25,
+      price: 150,
+      expiry: 2022,
+      status: true
     },
     {
-      name: "Laptop",
-      ProductId: 2,
-      price: 50000
+      id: 102,
+      name: 'Eltrombopag',
+      quantity: 90,
+      price: 550,
+      expiry: 2021,
+      status: true
     },
     {
-      name: "tv",
-      ProductId: 3,
-      price: 30000
+      id: 103,
+      name: 'Meloxicam',
+      quantity: 85,
+      price: 450,
+      expiry: 2025,
+      status: false
+    },
+    {
+      id: 104,
+      name: 'Allopurinol',
+      quantity: 50,
+      price: 600,
+      expiry: 2023,
+      status: true
+    },
+    {
+      id: 105,
+      name: 'Phenytoin',
+      quantity: 63,
+      price: 250,
+      expiry: 2021,
+      status: false
     }
   ];
 
-  // let sum = 0;
- 
-  // for (const i of myFunc) {
-  //    sum = sum + i.price;
-  // }
-  // console.log(sum);
+  const totalPrice = myFunc.reduce((acc , currentPrice) => {
+    return acc + currentPrice.price;
+  },0)
 
-  // const totalPrice = myFunc.reduce((acc , currentPrice) => {
-  //   return acc + currentPrice.price;
-  // },0)
-  // console.log(totalPrice);
-
-  let [a,b,c,d] = [1,2,3,4,5];
-  console.log(d); 
-
-  let {name,price,id} = {
-      name : "tv",
-      price : 50000,
-      id : 1
-  }
-  
-  console.log(tv);
+  const totalQuantity = myFunc.reduce((acc , currentquantity) => {
+    return acc + currentquantity.quantity;
+  },0)
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload on react-lite-1.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <table>
         <tbody>
           <tr>
+            <td>Table No :1</td>
+          </tr>          
+          <tr>
             <th>User Id</th>
-            <th>Title</th>
-            <th>Description</th>
+            <th>Name</th>
+            <th>Quantity</th>
             <th>Price</th>
+            <th>Expiry</th>
           </tr>
           {
-            // data.map((i) => {
-            //   return (
-            //     <tr key={Math.random(i)}>
-            //       <td>{i.id}</td>
-            //       <td>{i.title}</td>
-            //       <td>{i.description}</td>
-            //       <td>{i.price}</td>
-            //     </tr>
-            //   )
-            // })
+            myFunc.map((i) => {
+              return (
+                <tr key={Math.random(i)}>
+                  <td>{i.id}</td>
+                  <td>{i.name}</td>
+                  <td>{i.quantity}</td>
+                  <td>{i.price}</td>
+                  <td>{i.expiry}</td>
+                 
+                </tr>
+              )
+            })
           }
+          <tr>
+          <th>Total Price Is :- </th>
+          <th colSpan={4}>{totalPrice}</th>
+          </tr>
+          <th>Total Quantity Is :- </th>
+          <th colSpan={4}>{totalQuantity}</th>
+          
         </tbody>
       </table>
     </div>
