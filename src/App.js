61f8b28e0bbfc94ec99c90d823c99f8a7f6ef9bd
briz9343 <brijeshmavani2002@gmail.com>
2022-12-16 09:49:22 +0530

    circlet

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 
 function App() {
 
+  // Map And Reduce Method Use
   const myFunc = [
     {
       id: 101,
@@ -56,12 +57,61 @@ function App() {
     return acc + currentquantity.quantity;
   },0)
 
+
+  // Filter Method Use
+
+  const myFilter = [
+    {
+      name: "amit",
+      age: 35,
+      salary: 40000,
+      bonus: 1000,
+      status: true
+    },
+    {
+      name: "ajay",
+      age: 25,
+      salary: 38000,
+      bonus: 2000,
+      status: false
+    },
+    {
+      name: "mayur",
+      age: 23,
+      salary: 50000,
+      bonus: 500,
+      status: true
+    },
+    {
+      name: "jay",
+      age: 29,
+      salary: 35000,
+      bonus: 600,
+      status: true
+    },
+    {
+      name: "raj",
+      age: 33,
+      salary: 22000,
+      bonus: 2000,
+      status: true
+    },
+  ]
+
+  const salary = myFilter.filter((i) => i.salary >= 35000);
+
+  const lowSalary = myFilter.filter((i) => i.salary < 35000);
+
+
   return (
     <div className="App">
+
+      {/* Map And Reduce Method Use */}
       <table>
         <tbody>
           <tr>
-            <td>Table No :1</td>
+           
+            <th>Table No :1</th>
           </tr>          
           <tr>
             <th>User Id</th>
@@ -89,12 +139,67 @@ function App() {
           <th colSpan={4}>{totalPrice}</th>
           </tr>
           <th>Total Quantity Is :- </th>
-          <th colSpan={4}>{totalQuantity}</th>
-          
+          <th colSpan={4}>{totalQuantity}</th> 
         </tbody>
       </table>
-    </div>
-  );
-}
+      <br/>
+      <br/>
+      <br/>
 
+      {/* Map And Reduce Method Use */}
+      <table>
+        <tbody>
+          <tr>
+            <th>Table No :  2</th>
+
+          </tr>  
+          <tr>
+            <td>35000 Or Above Person</td>
+          </tr>        
+          <tr>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Salary</th>
+            <th>Bonus</th>
+          </tr>
+          {
+            salary.map((i) => {
+              return (
+                <tr key={Math.random(i)}>
+                  <td>{i.name}</td>
+                  <td>{i.age}</td>
+                  <td>{i.salary}</td>
+                  <td>{i.bonus}</td>
+                 
+                </tr>
+              )
+            })
+          }
+          <tr>
+            <td>Low Salary Person</td>
+          </tr>
+          <tr>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Salary</th>
+            <th>Bonus</th>
+          </tr>
+          {
+            lowSalary.map((i) => {
+              return (
+                <tr key={Math.random(i)}>
+                  <td>{i.name}</td>
+                  <td>{i.age}</td>
+                  <td>{i.salary}</td>
+                  <td>{i.bonus}</td>
+                 
+                </tr>
+              )
+            })
+          }
+          </tbody>
+        </table>
+    </div>
+  )
+}
 export default App;
